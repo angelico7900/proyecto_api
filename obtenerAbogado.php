8c4e5a587f324bf8e216d28542d1de0fa968330f
angelico7900 <57191784+angelico7900@users.noreply.github.com>
2021-05-04 09:23:36 +0200

    Update obtenerAbogado.php
    creado obtenerAbogado

--- obtenerAbogado.php
+++ obtenerAbogado.php
@@ -1,4 +1,16 @@
 <?php
-
-
+include($_SERVER['DOCUMENT_ROOT']."/api/usuarios/abogado.php");
+header('Access-Control-Allow-Origin: *');
+header('Acces-Control-Allow-Headers: Origin, X-Requested-With,Content-Type,Accept');
+$abogado = new Abogado();
+$datos = file_get_contents('php://input');
+$datos2 = json_decode($datos);
+$obAbogado = $abogado->getAbogado($datos->correo);
+$response = new stdClass();
+if(count($obAbogado) > 0){
+  $response->exito = 'OK';
+  $response->abogado = $obAbogado;
+}else{
+  $response->exito = 'ERR';
+echo(json_encode($response));
 ?>
