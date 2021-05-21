1ded943c2dd0c6a57a128fcc5f051353993c7181
angelico7900 <angelperezmartinez2000@gmail.com>
2021-05-21 03:08:01 +0200

    dockjh

--- enviarCorreoDocumento.php
+++ enviarCorreoDocumento.php
@@ -1,31 +1,39 @@
 <?php
+
 require_once('mail/PHPMailer.php');
-require_once('mail/SMTP.php');
-header('Access-Control-Allow-Origin: *');
-header('Acces-Control-Allow-Headers: Origin, X-Requested-With,Content-Type,Accept');
-$datos = file_get_contents("php://input");
-$datos2 = json_decode($datos);
-$mail = new PHPMailer\PHPMailer\PHPMailer();
+use PHPMailer\PHPMailer\PHPMailer;
+// require_once('mail/SMTP.php');
+// header('Access-Control-Allow-Origin: *');
+// header('Acces-Control-Allow-Headers: Origin, X-Requested-With,Content-Type,Accept');
+// $datos = file_get_contents("php://input");
+// $datos2 = json_decode($datos);
+$mail = new PHPMailer();
+$mail->isSMTP();
 $mail->SMTPAuth = true;
 $mail->SMTPSecure = 'tls';
 $mail->Host = "smtp.gmail.com";
 $mail->Port = 587;
 $mail->Password = "Aa12345678@";
 $mail->From = "angelperez2daw@gmail.com";
-$mail->FromName = "Angel";
-$mail->AddAddress($datos2->correo);
+$mail->FromName = "angelperez2daw@gmail.com";
+$mail->AddAddress("angelperezmartinez2000@gmail.com");
 $mail->Username = "angelperez2daw@gmail.com";
-$mail ->isHTML(true);
-$mail->Subject = "CONTRATO";
-$mail->Body = "<span>Le ha sido enviado su contrato</span>";
-$fichero = $datos2->fichero;
-$mail->addAttachment($fichero);
-$mail->TimeOut = 30;
+$mail->Subject = "contrato";
+$mail->Body = "Le ha sido enviado su contrato";
+// $fichero = $datos2->fichero;
+// foreach($fichero as $value){
+    //     // $mail->addAttachment($value);
+    // }
+    $mail->TimeOut = 30;
+    var_dump($mail);
+    $mail->Send();
+    echo("preuba");
 $response = new stdClass();
-if($mail->Send()){
-    $response->exito = 'OK';
-}else{
-    $response->exito = 'ERR';
-}
-echo(json_encode($response));
+// var_dump($mail);
+
+// if($mail->Send()){
+//     echo("saf");
+// }else{
+//     echo("sajflsaflñksj");
+// }
 ?>
